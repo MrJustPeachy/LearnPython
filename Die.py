2a7e6b07372391ce32c1f5b0688401dcc41228a0
peachy <dillonpie@gmail.com>
2018-10-13 23:10:08 -0600

    Created pseudocode for die.py

---
+++ Die.py
@@ -0,0 +1,19 @@
+"""
+Created by Dillon Pietsch and Akira Smith
+Created on 10/13/2018
+Purpose: Prints out a random number between 1-6 to simulate a roll of a die.
+Teaching examples: Shows use of library (random), user I/O, while & if loop
+"""
+
+# Import random module
+import random
+
+"""
+Pseudocode:
+    - Need to have function that randomly selects a number between 1-6
+    - Create a while loop
+        - Prompt user asking if they want to roll the dice (Y/N)
+            - If they want to roll the dice, call the random function
+            - Print result of function call
+        - If they want to quit, exit the program
+"""
